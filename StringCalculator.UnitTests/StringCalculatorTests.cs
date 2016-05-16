084c67d2964bd090e809169c4bd87b7ae0d4b989
Carlos Sanchez <Carlos.Sanchez@sbtpg.com>
2016-05-16 09:31:53 -0700

    start of repo

---
+++ StringCalculator.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculator.UnitTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void GivenAnEmptyString_WhenAdd_ThenReturnZero()
+        {
+            var result = new StringCalculator().Add(string.Empty);
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void GivenAStringWithOneNumber_WhenAdd_ThenReturnSum()
+        {
+            var result = new StringCalculator().Add("1");
+            result.Should().Be(1);
+        }
+
+        [Test]
+        public void GivenAStringWithTwoNumbersSeparatedByCommas_WhenAdd_ThenReturnSum()
+        {
+            var result = new StringCalculator().Add("1,2");
+            result.Should().Be(3);
+        }
+
+        [Test]
+        public void GivenAStringWithAnyNumbersSeparatedByCommasOrNewLine_WhenAdd_ThenReturnSum()
+        {
+            var result = new StringCalculator().Add("1\n2,3\n2,1");
+            result.Should().Be(9);
+        }
+
+        [Test]
+        public void GivenAStringWithADelimiterAndNumbersSeparatedByDelimiter_WhenAdd_ThenReturnSum()
+        {
+            var result = new StringCalculator().Add("//;\n1;2");
+            result.Should().Be(3);
+        }
+
+        [Test]        
+        public void GivenAStringContainingNegativeNumbers_WhenAdd_ThenThrowExceptionWithMessageContainingNegativeNumbers()
+        {
+            try
+            {
+                var result = new StringCalculator().Add("-1,2,3,-2");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("Negatives not allowed: -1, -2", ex.Message);    
+            }
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {      
+        public int Add(string input)
+        {
+            
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            var delimiter = GetDelimiter(ref input);
+
+            var numbers = input.Split('\n', delimiter);            
+
+            NegativeNumberCheck(numbers);
+
+            return numbers.Sum(Convert.ToInt32);
+        }
+
+        private static char GetDelimiter(ref string input)
+        {
+            var delimiter = ',';
+            if (input.StartsWith("//"))
+            {
+                delimiter = input[2];
+                input = input.Substring(input.IndexOf('\n') + 1);
+            }
+            return delimiter;
+        }
+
+        private void NegativeNumberCheck(string[] numbers)
+        {
+            List<string> negativeNumbers = new List<string>();
+
+            foreach (string number in numbers)
+            {
+                if (Convert.ToInt32(number) < 0)
+                {
+                    negativeNumbers.Add(number);
+                }
+            }
+
+            if (negativeNumbers.Count > 0)
+            {                
+                throw new ArgumentException("Negatives not allowed: " + string.Join(", ", negativeNumbers));
+            }            
+        }
+    }
+}
